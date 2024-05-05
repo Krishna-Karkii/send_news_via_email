f3f5dc5fa8505ada094210bf4f5ed12f44e090f3
krishna <krishnakarki1996@gmail.com>
2024-05-05 13:28:37 +0545

    add language, topic

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import send_email
 
+topic = "earth"
 # Api key and url
 api_key = "7ba2aaf2a0864e04a249eab3ea6ef225"
-url = ("https://newsapi.org/v2/everything?q=earth"
+url = (f"https://newsapi.org/v2/everything?q={topic}"
        "&from=2024-04-04&sortBy=publishedAt"
        "&apiKey=7ba2aaf2a0864e04a249eab3ea6ef225"
        "&language=en")
@@ -12,8 +13,8 @@ url = ("https://newsapi.org/v2/everything?q=earth"
 request = requests.get(url)
 df = request.json()
 
-content = "Subject: Today's news"
-for article in df["articles"][0:20]:
+content = "Subject: Today's news\n"
+for article in df["articles"][0:10]:
     if article["title"] and article["description"] is not None:
         content = (content + article['title']
                    + '\n' + article["description"]
